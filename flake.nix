3e17ebaabdc377f031f3997417ef26e969ae57f1
Giovanni d'Amelio <gio@damelio.net>
2023-05-06 16:42:05 -0700

    Install disko

--- flake.nix
+++ flake.nix
@@ -1,9 +1,15 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    disko = {
+      url = "github:nix-community/disko";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+      };
+    };
   };
 
-  outputs = { self, nixpkgs }: {
+  outputs = { self, nixpkgs, disko }: {
     colmena = {
       meta = {
         nixpkgs = import nixpkgs {
