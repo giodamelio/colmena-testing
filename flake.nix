474aed2ffe81e7214dbdd3f28d17519c938111b6
Giovanni d'Amelio <gio@damelio.net>
2023-05-06 16:42:57 -0700

    Remove Colmena for a bit

--- flake.nix
+++ flake.nix
@@ -10,27 +10,5 @@
   };
 
   outputs = { self, nixpkgs, disko }: {
-    colmena = {
-      meta = {
-        nixpkgs = import nixpkgs {
-          system = "x86_64-linux";
-          overlays = [];
-        };
-      };
-
-      host-a = { name, nodes, pkgs, ... }: {
-        boot.isContainer = true;
-        time.timeZone = nodes.host-b.config.time.timeZone;
-      };
-      host-b = {
-        deployment = {
-          targetHost = "somehost.tld";
-          targetPort = 1234;
-          targetUser = "luser";
-        };
-        boot.isContainer = true;
-        time.timeZone = "America/Los_Angeles";
-      };
-    };
   };
 }
