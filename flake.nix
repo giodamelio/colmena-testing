72d209fdf2dc05497d06fc5d85247420540d83e4
Giovanni d'Amelio <gio@damelio.net>
2023-05-06 17:42:08 -0700

    Generate images with nixos-generators

--- flake.nix
+++ flake.nix
@@ -7,8 +7,40 @@
         nixpkgs.follows = "nixpkgs";
       };
     };
+    nixos-generators = {
+      url = "github:nix-community/nixos-generators";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, disko }: {
+  outputs = { self, nixpkgs, disko, nixos-generators } @ inputs: let
+    lib = nixpkgs.lib;
+  in {
+    moduleLists = {
+      argon = [
+        disko.nixosModules.disko
+      ];
+    };
+
+    nixosConfigurations = {
+      argon = lib.nixosSystem {
+        system = "x86_64-linux";
+        specialArgs = inputs;
+        modules = self.moduleLists.argon;
+      };
+    };
+
+    packages.x86_64-linux = {
+      iso = nixos-generators.nixosGenerate {
+        system = "x86_64-linux";
+        format = "iso";
+        modules = self.moduleLists.argon;
+      };
+      proxmox-lxc = nixos-generators.nixosGenerate {
+        system = "x86_64-linux";
+        format = "proxmox-lxc";
+        modules = self.moduleLists.argon;
+      };
+    };
   };
 }
