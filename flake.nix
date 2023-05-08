6556705f833dcb8d9fa4c6d1fa55a011cd275e42
Giovanni d'Amelio <gio@damelio.net>
2023-05-08 16:01:55 -0700

    Things have happened...

--- flake.nix
+++ flake.nix
@@ -17,8 +17,52 @@
     lib = nixpkgs.lib;
   in {
     moduleLists = {
+      # A set of package to have in the installer iso
+      installer = [
+        ({ pkgs, ... }: {
+          environment.systemPackages = [
+            # Everyday helpful packages
+            pkgs.vim
+            pkgs.curl
+            pkgs.wget
+            pkgs.git
+            pkgs.ripgrep
+            pkgs.fd
+
+            # Partition management
+            disko.packages.x86_64-linux.disko
+
+          ] ++
+
+          # Scripts for each disk layout in disks/
+          (let
+            filesWithTypes = builtins.readDir ./disks;
+            files = lib.mapAttrsToList (name: _type: name) filesWithTypes;
+          in builtins.concatMap
+            (file:
+              let
+                nameWithoutNixSuffix = lib.removeSuffix ".nix" file;
+              in [
+                (pkgs.writeScriptBin
+                  "disko-${nameWithoutNixSuffix}-create"
+                  (disko.lib.create (import ./disks/${file} {}))
+                )
+                (pkgs.writeScriptBin
+                  "disko-${nameWithoutNixSuffix}-mount"
+                  (disko.lib.mount (import ./disks/${file} {}))
+                )
+              ]
+            ) files);
+
+          # Enable flakes
+          nix.settings.experimental-features = [ "nix-command" "flakes" ];
+        })
+      ];
+
+      # Minimal testing machine
       argon = [
         disko.nixosModules.disko
+        disko.lib.config (import ./disks.nix {})
         ({ pkgs, ... }: {
           environment.systemPackages = [
             pkgs.ripgrep
@@ -39,12 +83,7 @@
       isoInstaller = nixos-generators.nixosGenerate {
         system = "x86_64-linux";
         format = "install-iso";
-        modules = self.moduleLists.argon;
-      };
-      proxmox-lxc = nixos-generators.nixosGenerate {
-        system = "x86_64-linux";
-        format = "proxmox-lxc";
-        modules = self.moduleLists.argon;
+        modules = self.moduleLists.installer;
       };
     };
   };
